de9b55dc94ce2a5c0e7b3129d0f3e1c88e08a753
Andrea Castro <ap.castrocastellon@gmail.com>
2021-09-29 01:41:38 -0300

    first commit

---
+++ firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID
+};
+
+export default firebaseConfig;
